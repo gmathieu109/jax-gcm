Fix sim_time overflow issue (#369)
* Fix sim_time overflow issue

* Actually, fix the overflow without using int64

* Add test for date overflow

jcm/model.py
@@ -347,7 +347,10 @@ class Model:
 
     def _date_from_sim_time(self, sim_time) -> DateData:
         return DateData.set_date(
-            model_time=self.start_date + jdt.Timedelta(seconds=jnp.round(sim_time).astype(jnp.int32)),
+            model_time=self.start_date + jdt.Timedelta(
+                days=jnp.floor(sim_time / 86400).astype(jnp.int32),
+                seconds=jnp.round(sim_time % 86400).astype(jnp.int32)
+            ),
             model_step=jnp.int32(sim_time / self.dt_si.m),
             dt_seconds=self.dt_si.m
         )